83027ce440dda5644b757607513eac65e2427f71
BegoCaZa <begoxcz@hotmail.com>
2025-05-26 17:17:30 +0200

    destinations page

--- src/pages/destination/Destination.jsx
+++ src/pages/destination/Destination.jsx
@@ -1,4 +1,9 @@
+import { useState } from 'react';
+import { DESTINATIONS } from '../../constants/destinations';
+
 const Destination = () => {
+  const [activeDestination, setActiveDestination] = useState(1); //que empiece con la luna
+  const currentDestination = DESTINATIONS[activeDestination];
   return (
     <>
       <h2>DESTINATION:l</h2>

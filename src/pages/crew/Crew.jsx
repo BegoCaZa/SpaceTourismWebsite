62b10a4a8c527a9ac3a277556dade5e693b1c0df
BegoCaZa <202084412+BegoCaZa@users.noreply.github.com>
2025-05-27 11:56:47 +0200

    update

--- src/pages/crew/Crew.jsx
+++ src/pages/crew/Crew.jsx
@@ -1,8 +1,24 @@
+import { useState } from 'react';
+import { CREW } from '../../constants/crew';
+
 const Crew = () => {
+  const [activeCrewMember, setActiveCrew] = useState(CREW[0].id);
+
+  const currentCrewMember = CREW.find(crew => crew.id === activeCrewMember);
+
+  console.log('CREW MEMBER:', CREW);
+  console.log('activeCrewMember:', activeCrewMember);
   return (
-    <>
-      <h2>CREW</h2>
-    </>
+    <StyledPageContainer>
+      <StyledPageHeaderTitle>
+        <StyledPageTitleNumber>02</StyledPageTitleNumber>
+        <span>MEET YOUR CREW</span>
+      </StyledPageHeaderTitle>
+
+      <StyledImageContainer>
+        <StyledCrewImage src={currentCrewMember.image} />
+      </StyledImageContainer>
+    </StyledPageContainer>
   );
 };
 export default Crew;
---
+++ src/pages/crew/crew.styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+import { COLORS } from '../../styles/Colors';
+import { TYPOGRAPHY } from '../../styles/Typography';
+import { TYPOGRAPHY_FONTS } from '../../styles/Typography';
+import { TYPOGRAPHY_WEIGHTS } from '../../styles/Typography';
